7a7607e783f6797048819d296891b1aab912d79a
Fizza-A786 <atesh7865@gmail.com>
2026-04-21 02:09:39 -0600

    up

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import productReducer from "./Features/ProductSlice";
+import productReducer from "./Features/ProductSlice";
 import cartReducer from "./Features/CartSlice";
 
 export const store = configureStore({
   reducer: {
-    // products: productReducer,
+    products: productReducer,
     cart: cartReducer,
   },
 });
